f8143080b1de24e6c3f6f097247a326925e4d1ee
JoHigashiyama <j.higashiyama.sys23@morijyobi.ac.jp>
2025-09-05 09:59:38 +0900

    プロジェクトの作成

---
+++ src/main/java/com/example/sortquiz/SortQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.sortquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortQuizApplication.class, args);
+    }
+
+}
