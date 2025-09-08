db27c7740d4941e5106bd838cc9533349475bcf7
JoHigashiyama <j.higashiyama.sys23@morijyobi.ac.jp>
2025-09-08 10:19:06 +0900

    ユーザーの登録処理とルーティングの実装

--- src/main/java/com/example/sortquiz/controller/RegistrationController.java
+++ src/main/java/com/example/sortquiz/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.example.sortquiz.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
@@ -17,5 +18,11 @@ public class RegistrationController {
     public String showRegistrationForm(Model model) {
         model.addAttribute("userForm", new UserForm());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(UserForm userForm) {
+        userService.createUser(userForm);
+        return "redirect:/login?register";
     }
 }
